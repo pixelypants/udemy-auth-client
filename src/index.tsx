6255a1a371c75830177424ec1778d23b083cafe3
Hudson Graham <hudson.graham@gmail.com>
2019-02-11 20:53:50 +1000

    Add redux-thunk

--- src/index.tsx
+++ src/index.tsx
@@ -4,10 +4,17 @@ import { BrowserRouter, Route, withRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware } from "redux";
 import reducers from "./store";
+import reduxThunk from "redux-thunk";
 
 import App from "./components/App";
 import Welcome from "./components/Welcome";
 import { Signup } from "./components/auth/Signup";
+
+const store = createStore(
+    reducers,
+    {},
+    applyMiddleware(reduxThunk)
+);
 
 ReactDOM.render(
     <Provider store={createStore(reducers, {})}>
